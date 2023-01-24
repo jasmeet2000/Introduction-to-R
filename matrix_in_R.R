89cf4eb9de0599b942ff2175e05a171ba4f43f58
jasmeet2000 <sainismarty0707@gmail.com>
2023-01-24 00:30:29 -0500

    Matrix and R markdown basics

---
+++ matrix_in_R.R
@@ -0,0 +1,12 @@
+# Matrix
+# In R, a matrix is a collection of elements of the same data type 
+# (numeric, character, or logical) arranged into a fixed number of rows and
+# columns. Since we are only working with rows and columns, a matrix is 
+# called two-dimensional.
+
+# We can construct a matrix in R with the matrix() function.
+
+# Construct a matrix with 3 rows that contain the numbers 1 up to 9
+matrix(1:9, byrow = TRUE, nrow = 3)
+
+#______________________________________________________________________
