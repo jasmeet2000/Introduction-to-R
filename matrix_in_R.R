88966039728e82c131a13b98c3f671cd57c78414
jasmeet2000 <sainismarty0707@gmail.com>
2023-01-28 00:14:07 -0500

    Matrix in R with example

--- matrix_in_R.R
+++ matrix_in_R.R
@@ -12,3 +12,68 @@ matrix(1:9, byrow = TRUE, nrow = 3)
 #______________________________________________________________________
 
 matrix <- matrix(1:9, byrow = TRUE, nrow = 3)
+
+
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Construct star_wars_matrix
+box_office <- c(460.998, 314.4, 290.475, 247.900, 309.306, 165.8)
+region <- c("US", "non-US")
+titles <- c("A New Hope", 
+            "The Empire Strikes Back", 
+            "Return of the Jedi")
+
+star_wars_matrix <- matrix(box_office, 
+                           nrow = 3, byrow = TRUE,
+                           dimnames = list(titles, region))
+
+box_office2 <- c(474.5, 314.4, 310.7, 552.5, 380.3, 468.5)
+titles <- c("The Phantom Menace", 
+            "Attack of the Clones", 
+            "Revenge of the Sith")
+star_wars_matrix2 <- matrix(box_office2, 
+                           nrow = 3, byrow = TRUE,
+                           dimnames = list(titles, region))
+
+# The worldwide box office figures
+worldwide_vector <- rowSums(star_wars_matrix)
+
+# Bind the new variable worldwide_vector as a column to star_wars_matrix
+all_wars_matrix <- cbind(star_wars_matrix,worldwide_vector)
+
+# Combine both Star Wars trilogies in one matrix
+# star_wars_matrix2 same as star_wars_matrix
+all_wars_matrix <- rbind(star_wars_matrix,star_wars_matrix2)
+
+# Total revenue for US and non-US
+total_revenue_vector <- colSums(all_wars_matrix)
+
+# Print out total_revenue_vector
+total_revenue_vector
+
+# _________________________________________________________________________
+# Selection of matrix elements
+
+# all_wars_matrix is available in your workspace
+all_wars_matrix
+
+# Select the non-US revenue for all movies
+non_us_all <- all_wars_matrix[,2]
+
+# Average non-US revenue
+mean(non_us_all)
+
+# Select the non-US revenue for first two movies
+non_us_some <- all_wars_matrix[1:2,2]
+
+# Average non-US revenue for first two movies
+mean(non_us_some)
+
+# Estimate the visitors
+visitors <- all_wars_matrix / 5
+
+# Print the estimate to the console
+visitors
