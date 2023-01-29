73596daad8984a9e4ccea6058b2060c6775e5fc0
jasmeet2000 <sainismarty0707@gmail.com>
2023-01-29 00:02:28 -0500

    Data Frames in R

---
+++ data_frame_in_r.R
@@ -0,0 +1,17 @@
+# When doing a market research survey, however, you often have questions such as:
+#   'Are you married?' or 'yes/no' questions (logical)
+# 'How old are you?' (numeric)
+# 'What is your opinion on this product?' or other 'open-ended' questions (character)
+
+#A data frame is a table or a two-dimensional array-like structure in which each column
+#contains values of one variable and each row contains one set of values from each column.
+
+# Print out built-in R data frame
+mtcars 
+
+# Call head() on mtcars
+head(mtcars)
+tail(mtcars)
+
+# Investigate the structure of mtcars
+str(mtcars)
