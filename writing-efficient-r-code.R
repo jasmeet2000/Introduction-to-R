32a869425469ddfbb62d4415e1d2d59ced49fc8d
jasmeet2000 <sainismarty0707@gmail.com>
2023-01-31 22:32:14 -0500

    basic in R

---
+++ writing-efficient-r-code.R
@@ -0,0 +1,32 @@
+# Writing Efficient r code
+
+# Print the R version details using version
+version
+
+# Assign the variable major to the major component
+major <- version$major
+major
+
+# Assign the variable minor to the minor component
+minor <- version$minor
+minor
+
+#Benchmarking in R
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
