f6b669aa6a0c8918d2f78fdf4f901e90aaa35a01
jasmeet2000 <sainismarty0707@gmail.com>
2023-02-01 23:48:47 -0500

    changes in benchmark

--- writing-efficient-r-code.R
+++ writing-efficient-r-code.R
@@ -13,7 +13,23 @@ minor
 
 #Benchmarking in R
 
+# 1. We construct a function around the feature we wish to benchmark
+# 2. We time the function under different scenario, eg., data-set
 
+#-----------------------------------------------------------------------
+#Example
+# Comparing read times of CSV and RDS files
+# How long does it take to read movies from CSV?
+system.time(read.csv("movies.csv"))
+
+# How long does it take to read movies from RDS?
+system.time(readRDS("movies.rds"))
+
+#-----------------------------------------------------------------------
+# There are a number of ways to assign variables to objects. The two standard ways
+# are to use the = or <- operators. Which of the following statements is true?
+# Ans:- Using the <- operator inside a function call will create a new (or overwrite 
+                                                                # an existing) object.
 
 
 
