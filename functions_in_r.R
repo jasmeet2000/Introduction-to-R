a12c72c75f3ca13c460752fe0e98752543702f02
jasmeet2000 <sainismarty0707@gmail.com>
2023-02-10 23:25:13 -0500

    changes

--- functions_in_r.R
+++ functions_in_r.R
@@ -154,7 +154,25 @@ toss_coin <- function(n_flips) {
 toss_coin(n_flips=10)
 
 
+#_______________________________________________________________________________
+# Set the categories for interval_type to "(lo, hi]" and "[lo, hi)"
+cut_by_quantile <- function(x, n = 5, na.rm = FALSE, labels = NULL, 
+                            interval_type= c("(lo, hi]", "[lo, hi)") ) {
+  # Match the interval_type argument
+  interval_type <- match.arg(interval_type)
+  probs <- seq(0, 1, length.out = n + 1)
+  qtiles <- quantile(x, probs, na.rm = na.rm, names = FALSE)
+  right <- switch(interval_type, "(lo, hi]" = TRUE, "[lo, hi)" = FALSE)
+  cut(x, qtiles, labels = labels, right = right, include.lowest = TRUE)
+}
 
+# Remove the interval_type argument from the call
+cut_by_quantile(n_visits)
+#-------------------------------
+# Clever categorical default setting! As a bonus, match.arg() handles throwing an 
+# error if the user types a value that wasn't specified.
 
+# Match the argument
+?match.arg()
 
-
+#_______________________________________________________________________________
