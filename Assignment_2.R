6ebf46ea6416e68dd3f3872fd5a4fce234b2a839
jasmeet2000 <sainismarty0707@gmail.com>
2023-02-11 21:05:18 -0500

    changes

--- Assignment_2.R
+++ Assignment_2.R
@@ -8,23 +8,24 @@ car_dataset <- mtcars
 
 #a)  What is the average miles per gallon for all vehicles in the mtcars database (1 marks)?
 mean_mpg <- mean(car_dataset$mpg)
-round(mean_mpg,4)
+round(mean_mpg,2)
 
 
 # b) What is the standard deviation of the number of cylinders for all vehicles in the mtcars database
 # (1 marks)?
 sd_cyl <- sd(car_dataset$cyl)
-round(sd_cyl,4)
+round(sd_cyl,2)
 
 # c) If you were to choose a measure of central tendency for the gross horsepower across all vehicles in the 
 # mtcars database, would you choose the mean or the median? Why? (2 marks)
 
 # ANS:- 
 hist(car_dataset$hp)
+hist(car_dataset$hp, main = "Histogram of Gross Horsepower", xlab = "Gross Horsepower (hp)", ylab = "Number of Vehicles", col = 4)
+boxplot(car_dataset$hp, main = "Boxplot of Gross Horsepower", ylab = "Gross Horsepower (hp)", col = "grey")
 boxplot(car_dataset$hp)
 
-median_hp <-  median(car_dataset$hp)
-round(median_hp,4)
+median_hp <- round(median(car_dataset$hp),4)
 
 # d) If you were to choose a measure of variability for the displacement in cubic inches across all vehicles
 #   in the mtcars database, would you choose the standard deviation or the interquartile range? Why? (2 marks)
@@ -32,12 +33,17 @@ round(median_hp,4)
 
 boxplot(car_dataset$disp)
 hist(mtcars$disp)
+
+hist(car_dataset$disp, main = "Histogram of Displacement", xlab = "Displacement (in cubic inches)",ylab = "Number of Vehicles", col = "2")
+boxplot(car_dataset$disp, main = "Boxplot of Displacement", ylab = "Displacement (in cubic inches)",col = "pink")
+
 # IQR
 q3 <- 326
 q1  <- 120.825
 q3 -q1
 quantile(mtcars$disp)
 IQR(mtcars$disp)
+sd(mtcars$disp)
 
 #______________________________________________________________________________________________________________
 
@@ -54,15 +60,15 @@ orange_dataset <- Orange
 
 args(plot)
 plot(orange_dataset$age, orange_dataset$circumference
-     , xlab = "Age of trees(in days)"
+     , xlab = "Age of trees (in days)"
      , ylab = "Circumference of trees (in mm)"
      ,main= "Relationship between tree age \nand the trunk circumference"
      , col="darkgreen"
      , cex=1.2
-     , pch="+") 
+     , pch="o") 
 
 
-# Describe the relationship presented in your plot in the following terms and justify your answer: (3 marks)
+  # Describe the relationship presented in your plot in the following terms and justify your answer: (3 marks)
 
 # A) Is the relationship positive, negative, or is there no relationship?
 # Ans: positive
@@ -72,16 +78,21 @@ plot(orange_dataset$age, orange_dataset$circumference
 
 # C) Is the relationship weak, moderate, or strong?
 # ANS: strong
+corr(orange_dataset$age, orange_dataset$circumference)
 
 #_______________________________________________________________________________________________________________
 
 # The following items require you to use the table to the right.  (6 marks)
 
-sex <- factor(c(rep(x="Male",times=8),rep(x="Female",times=8)), levels = c("Male","Female"))
+sex <- factor(c(rep(x="Male",times=8)
+                , rep(x="Female",times=8))
+                , levels = c("Male","Female"))
 sex
 
-major <- factor(c("Psychology","Physics","Psychology","Biology","Chemistry","Biology","Biology","Physics",
-                  "Physics","Biology","Physics","Psychology","Chemistry","Physics","Psychology","Biology")
+major <- factor(c("Psychology","Physics","Psychology","Biology"
+                  , "Chemistry","Biology","Biology","Physics"
+                  , "Physics","Biology","Physics","Psychology"
+                  , "Chemistry","Physics","Psychology","Biology")
                 , levels = c("Biology","Chemistry","Physics","Psychology"))
 major
 
@@ -104,7 +115,7 @@ student_table <- table(sex,major)
 student_table
 
 ?prop.table
-student_table_proportions <- round(prop.table(student_table, 1)*100,2)
+student_table_proportions <- round(prop.table(student_table, NULL)*100,2)
 # (student_table[1,1]/5)*100
 student_table_proportions
 
@@ -113,10 +124,16 @@ student_table_proportions
 
 ?barplot   # dodged  --> Where bars are broken up and placed beside each other.
 
+par(mar=c(4,4,4,4))
 barplot(student_table_proportions,beside=FALSE,horiz=FALSE,las=1
-        ,names.arg=levels(major), legend.text=levels(sex), main = "PROPORTIONS OF EACH MAJOR SPLIT \nBY GENDER"
-        ,xlab ="MAJORS",ylab = "FREQUENCY", ylim = c(0,100), col = c("blue","pink"))
+        ,names.arg=levels(major), legend.text=levels(sex)
+        , main = "PROPORTIONS OF EACH MAJOR SPLIT \nBY GENDER"
+        , xlab ="MAJORS",ylab = "FREQUENCY", col = c("blue","pink")
+        , args.legend=list(x="topright"))
 
+plot(sex,major,col = c("blue", "pink"), xlab ="MAJORS",ylab = "FREQUENCY")
+
+student_table_proportions[major]
 # c) Without performing further analysis, describe your observations of the differences in majors
 #    between men and women. What do you see when you look at this plot? (2 marks)
 
